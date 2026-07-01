05e78947afbdfd95082af0302868fd8110d49a6a
chaturved <66498683+chaturved@users.noreply.github.com>
2026-07-01 15:03:40 -0500

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,36 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TreeNode()
+            curr = curr.children[char]
+
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+
+        return curr.end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+
+        return True
+
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
