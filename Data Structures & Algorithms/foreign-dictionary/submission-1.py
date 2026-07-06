71b8c227000c767c455b85c411aacb49b544f369
chaturved <66498683+chaturved@users.noreply.github.com>
2026-07-05 20:37:33 -0500

    Add: foreign-dictionary - submission-1

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+
+        for i in range(len(words) - 1):
+            word1, length1 = words[i], len(words[i])
+            word2, length2 = words[i + 1], len(words[i + 1])
+
+            found = False
+            for j in range(min(length1, length2)):
+                if word1[j] != word2[j]:
+                    adj[word1[j]].add(word2[j])
+                    found = True
+                    break
+            
+            if found or word2.startswith(word1):
+                continue
+            
+            return ""
+        
+        result = []
+        def dfs(u):
+            if u in global_visited:
+                return True
+            
+            if u in visited:
+                return False
+            
+            visited.add(u)
+            
+            for v in adj[u]:
+                if not dfs(v):
+                    return False
+            
+            global_visited.add(u)
+            result.append(u)
+            return True
+
+        global_visited = set()
+        for u in adj:
+            if u in global_visited:
+                continue
+            
+            visited = set()
+            if not dfs(u):
+                return ""
+
+        return "".join(result[::-1])
