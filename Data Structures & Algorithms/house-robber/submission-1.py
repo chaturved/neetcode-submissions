412fbeff6242b151a1700578e075a8fd9e33a970
chaturved <66498683+chaturved@users.noreply.github.com>
2026-07-07 13:36:27 -0500

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        define amount[i] is max amount that can be made at ith house
+        two possibilites: it can be either robbing ith and not robbing
+
+        robbing ith means amount[i] = robbing amount[i - 2] + cost[i]
+        skipping means just amount[i - 1]
+
+        amount[i] = max(amount[i - 2] + cost[i], amount[i - 1])
+        """
+
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        amount = [0] * n
+        amount[0] = nums[0]
+        amount[1] = max(amount[0], nums[1])
+
+        for i in range(2, n):
+            amount[i] = max(amount[i - 2] + nums[i], amount[i - 1])
+        
+        return amount[n - 1]
+
