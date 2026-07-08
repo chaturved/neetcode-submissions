48ade0620e23abcc92fe6d553873445b22fecce4
chaturved <66498683+chaturved@users.noreply.github.com>
2026-07-08 17:12:23 -0500

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = { len(s): 1 }
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+
+            if s[i] == "0":
+                dp[i] = 0
+                return 0
+            
+            ways = dfs(i + 1)
+
+            if i + 2 <= len(s) and 10 <= int(s[i: i + 2]) <= 26:
+                ways += dfs(i + 2)
+            
+            dp[i] = ways
+            return ways
+        
+        return dfs(0)
