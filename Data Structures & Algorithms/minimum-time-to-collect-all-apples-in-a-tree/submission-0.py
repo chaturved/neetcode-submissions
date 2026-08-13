bfd8c8834cdc78828aff0c4a7ac80cae33352195
chaturved <66498683+chaturved@users.noreply.github.com>
2026-08-12 21:37:59 -0500

    Add: minimum-time-to-collect-all-apples-in-a-tree - submission-0

---
+++ Data Structures & Algorithms/minimum-time-to-collect-all-apples-in-a-tree/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append((v, hasApple[v]))
+            adj[v].append((u, hasApple[u]))
+        
+        def dfs(u, p):
+            total_time = 0
+
+            for v, has_apple in adj[u]:
+                if v == p:
+                    continue
+
+                time_taken = dfs(v, u)
+                if has_apple or time_taken > 0:
+                    total_time += time_taken + 2
+                    
+            return total_time
+        
+        return dfs(0, 0)
+
+        
