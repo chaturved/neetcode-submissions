47df902ccf0c1baa3a4c95bc10dac3d0f3532e5d
chaturved <66498683+chaturved@users.noreply.github.com>
2026-08-24 19:18:54 -0500

    Add: redistribute-characters-to-make-all-strings-equal - submission-0

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        freq = [0] * 26
+        for word in words:
+            for char in word:
+                freq[ord(char) - ord('a')] += 1
+        
+        for count in freq:
+            if count % len(words) != 0:
+                return False
+        
+        return True
